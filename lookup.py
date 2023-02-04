5f90d9a2b2ea9582153c32e413fbc48554f21829
Albin Touma <albin.touma@gmail.com>
2023-02-04 20:49:22 +0200

    Corrected JSON, added init file, added input function to lookup verb conjugations in present tense

--- lookup.py
+++ lookup.py
@@ -1,4 +1,14 @@
 import json
 import pandas as pd
+from resources import *
 
-print('hj')
+
+for  verb in RomanianDictionary['Verbs']:
+    if (verb['Infinitive'] == input(str)): #input infinitive of verb you want to search
+        verb = verb['Infinitive'], verb['Definition'], verb['Simple Present']
+        print(json.dumps(verb, indent=4))
+    else:
+        if not(verb['Infinitive'] == input(str)):
+            print('error')
+            exit()
+
---
+++ resources/__init__.py
@@ -0,0 +1,3 @@
+import json
+RomanianDictionary = open("C:/Users/albin/Documents/python_projects/Romanian/resources/RomanianData.json")
+RomanianDictionary = json.load(RomanianDictionary)
